2b25269de735a4671d3d728b4b368089503dd319
Web Coders <webcoders1122@gmail.com>
2024-02-26 09:07:14 +0500

    initial code with firebase setup

---
+++ src/context/Firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADkNd-7OsztV-y6HXMRx1SiEnjRZAZFYM",
+  authDomain: "book-store-49549.firebaseapp.com",
+  projectId: "book-store-49549",
+  storageBucket: "book-store-49549.appspot.com",
+  messagingSenderId: "937966815181",
+  appId: "1:937966815181:web:98c244950c0356517a2684",
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
